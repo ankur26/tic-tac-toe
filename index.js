f932f2eb5a221bc2263ef6f9fe8f1182359c0c1b
Ankur Bhatkalkar <ankur26@users.noreply.github.com>
2021-12-03 21:23:11 -0500

    Added TTT logic module. included script and css in html

--- index.js
+++ index.js
@@ -1,26 +1,73 @@
 const GameBoard = ( () => {
-    const gameBoard = new Array(9);
-    const roundover = false;
-    const winner = "";
+    let gameBoard = ["","","","","","","","",""];
+    let roundover = false;
+    let winner = "";
+    const roundOver = () => roundover;
+    const Winner = () => winner;
     const refresh = () => {
-        this.gameBoard = new Array(9);
-        this.roundover = false;
-        this.winner = "";
+        gameBoard = ["","","","","","","","",""];
+        roundover = false;
+        winner = "";
     }
     const checkGridFilled = () => {
-        this.gameBoard.forEach(element => {
-            if(!element) return false;
+        let filled = true;
+        gameBoard.forEach((element,index) => {
+            // console.log({index,element});
+            if(element === "") filled = false;
         });
-        return true;
+        return filled;
+        // console.log(gameBoard);
+    }
+    const winningCondidtionCheck = (string) =>{
+        if(
+            (string.includes("0") && string.includes("1") && string.includes("2"))  || 
+            (string.includes("3") && string.includes("4") && string.includes("5")) || 
+            (string.includes("6") && string.includes("7") && string.includes("8")) ||
+            (string.includes("0") && string.includes("3") && string.includes("6")) ||
+            (string.includes("1") && string.includes("4") && string.includes("7")) ||
+            (string.includes("2") && string.includes("6") && string.includes("8")) ||
+            (string.includes("0") && string.includes("4") && string.includes("8")) ||
+            (string.includes("2") && string.includes("4") && string.includes("6"))
+        ){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     const checkWinner = () => {
-        
+        let xString = "";
+        let oString = "";
+        gameBoard.forEach((element,index)=>{
+            if (element){
+                if (element === "X" ) xString+=`${index}`;
+                else if(element === "O" ) oString+=`${index}`;
+            }
+        });
+        if(winningCondidtionCheck(xString)){
+            roundover = true;
+            winner = "X";
+        }else if(winningCondidtionCheck(oString)){
+            roundover = true;
+            winner = "O"
+        }else if(checkGridFilled() && !winningCondidtionCheck(xString) && !winningCondidtionCheck(oString)){
+            roundover = true;
+            winner = "draw";
+        }
     }
     const updateGrid = (location,value) =>{
-        this.gameBoard[location-1]=value;
-        this.checkWinner();
+        if(gameBoard[location-1] === ""){
+            gameBoard[location-1]=value;
+            checkWinner();
+            return true;
+        }else{
+            console.log("Wrong move, someone has already got a hold of that grid");
+            return false;
+        }
     }
-
+    return {roundOver,Winner,updateGrid,refresh};
 })();
+
+
 
 // console.log(GameBoard());
